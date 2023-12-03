b4fcafc271e6499e6e8a2e5a44aa8c693b8d3564
锐源 <1370164516@qq.com>
2023-12-03 13:51:01 +0800

    less编译

--- README.md
+++ README.md
@@ -3,7 +3,14 @@
 - npm
 - gulp 前端构建工具  npm install --global gulp
 
+## 1-1、sass和less都是css预处理器，提供了css不具备的功能，如变量、嵌套和混合等；
+- sass功能更多，less和css写法相似，更容易学,简单易用，适合小项目
+- sass是ruby编写；less是js编写
+- 
+
 # 2、使用gulp编译sass
 - 在laravel框架：npm install 安装相关依赖
 - 根目录创建gulpfile.js文件，主要用于定义自动化构建任务
 - gulp 编译完会在public生成相应的前端文件
+
+# 3、使用Elixir编译Less
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var elixir = require('laravel-elixir');
 
 
+// elixir.config.assetsPath = 'assets'// ('指定读取编译的文件路径');
 elixir(function (mix) {
     //格式一
     // mix.sass(['app.scss', 'front.scss'], 'public/styles/style.css');
 
     //格式二
-    mix.sass('app.scss', 'public/styles/style.css')
-        .sass('front.scss', 'public/styles/front.css')
+    // mix.sass('app.scss', 'public/styles/style.css')
+    //     .sass('front.scss', 'public/styles/front.css')
+
+    mix.less(['app.scss', 'front.scss'], 'public/styles/style.css');
 });
